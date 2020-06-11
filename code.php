cbb566fa629132d6c624f5055ac49b48dab02256
sejal1126 <61845736+sejal1126@users.noreply.github.com>
2020-06-12 01:41:41 +0530

    Update code.php

--- code.php
+++ code.php
@@ -1,8 +1,6 @@
-<html><head><title><?php echo "DONUTS";?></title></head>
-<body
+
 <?php 
 print "DONUT WEBSITE"; 
 print 'ifconfig'      
-$image_url='https://i.imgur.com/keNc6iz.jpeg';
 ?>
-<br><h1>ENJOY IT GUYS!!?</h1>
+
