e2fa747086eeffd87a04085d468fa867abc57964
sejal1126 <61845736+sejal1126@users.noreply.github.com>
2020-06-12 01:09:34 +0530

    Create code.php

---
+++ code.php
@@ -0,0 +1,10 @@
+<html><head><title><?php echo "DONUTS";?></title></head>
+<body
+<?php 
+print "DONUT WEBSITE"; 
+$image_url='https://i.imgur.com/keNc6iz.jpeg';
+?>
+
+<img src="<?php echo $image_url;?>">
+
+<br><h1>ENJOY IT GUYS!!?</h1>
